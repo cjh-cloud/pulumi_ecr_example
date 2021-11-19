8dc0e05d1ac801658077a8f1e71de2eb0809abb5
SenorGrande <clooneyjh@gmail.com>
2021-11-19 18:10:06 +1300

    feat: part 1 pulumi project to create an ecr repo and push a docker image to it

---
+++ index.ts
@@ -0,0 +1,23 @@
+
+import * as aws from "@pulumi/aws";
+import * as docker from "@pulumi/docker";
+import * as pulumi from "@pulumi/pulumi";
+
+// Create a private ECR repository.
+const repo = new aws.ecr.Repository("my-repo");
+
+// Get registry info (creds and endpoint)
+const imageName = repo.repositoryUrl;
+
+const customImage = "my-image"; // name of the pulumi resource
+const imageVersion = "v1.0.1"; // todo : grab from an env var, created in pipeline by semantic release
+
+// Build and publish the container image.
+const image = new docker.Image(customImage, {
+	build: "app",
+	imageName: pulumi.interpolate`${imageName}:${imageVersion}`,
+});
+
+// Export the base and specific version image name.
+export const baseImageName = image.baseImageName;
+export const fullImageName = image.imageName;
